96feecf17e0ef33f363a4a73fb883e8670ad1062
AA001KK <kamron.atabayev@gmail.com>
2025-03-20 16:29:14 +0500

    ..............

--- Haval/controllers/authController.js
+++ Haval/controllers/authController.js
@@ -6,159 +6,164 @@ require("dotenv").config();
 const { validationResult } = require("express-validator");
 
 exports.register = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        
-        const { name, email, password, role } = req.body;
- 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(409).json({
-                error: "Bu email bilan foydalanuvchi allaqachon mavjud",
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        
-        const user = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-            role: role || "user",
-        });
-
-        res.status(200).json({
-            message: "Foydalanuvchi muvaffaqiyatli ro‘yxatdan o‘tdi.",
-            user,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Serverda xatolik yuz berdi." });
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        error: "Bu email bilan foydalanuvchi allaqachon mavjud",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: role || "user",
+    });
+
+    res.status(200).json({
+      message: "Foydalanuvchi muvaffaqiyatli ro‘yxatdan o‘tdi.",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Serverda xatolik yuz berdi." });
+  }
 };
 
 exports.login = async (req, res) => {
-    try {
-    const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user) return res.status(404).json({ message: 'Foydalanuvchi topilmadi' });
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return res.status(400).json({ message: 'Noto‘g‘ri parol' });
-  
-      const token = jwt.sign(
-        { 
-            id: user._id, 
-            email: user.email,
-            name: user.name,
-            role: user.role 
-        }, 
-        process.env.JWT_SECRET_KEY, 
-        { expiresIn: '1d' });
-
-    return res.status(200).send({
-        token,
-        userId: user._id
-    });
-    } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error: error.message });
-    }
-  };
-
-
-exports.loginAdmin = async (req, res) => {
-    console.log(req.body);
-
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
-
     const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(404).json({ message: "Foydalanuvchi topilmadi" });
 
-    try {
-        const admin = await Admin.findOne({ email: email });
-        console.log(admin);
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Noto‘g‘ri parol" });
 
-        if (!admin) {
-            return res.status(404).send("Admin not found");
-        }
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
 
-        const isPasswordValid = await bcrypt.compare(password, admin.password);
-        if (!isPasswordValid) {
-            return res.status(401).send("Invalid credentials");
-        }
+    return res.status(200).send({
+      token,
+      userId: user._id,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
+};
 
-        const token = jwt.sign(
-            {
-                id: admin._id,
-                email: admin.email,
-                adminName: admin.adminName,
-                role: admin.role,
-            },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: "1d" }
-        );
+exports.loginAdmin = async (req, res) => {
+  console.log(req.body);
 
-        return res.status(200).send({
-            token,
-        });
-    } catch (error) {
-        console.error("Error during login:", error);
-        return res.status(500).json({ error: "Server xatosi yuz berdi." });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email: email });
+    console.log(admin);
+
+    if (!admin) {
+      return res.status(404).send("Admin not found");
     }
+
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+    if (!isPasswordValid) {
+      return res.status(401).send("Invalid credentials");
+    }
+
+    admin.lastLogin = new Date();
+    await admin.save();
+
+    const token = jwt.sign(
+      {
+        id: admin._id,
+        email: admin.email,
+        adminName: admin.adminName,
+        role: admin.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).send({
+      token,
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Server xatosi yuz berdi." });
+  }
 };
 
 exports.loginSuperAdmin = async (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email: email });
+
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
     }
 
-    const { email, password } = req.body;
-
-    try {
-        const admin = await Admin.findOne({ email: email });
-
-        if (!admin) {
-            return res.status(404).json({ error: "Admin not found" });
-        }
-
-        if (admin.role !== "superadmin") {
-            return res.status(403).json({ error: "Sizga ruxsat yo‘q!" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, admin.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: "Invalid credentials" });
-        }
-
-        const token = jwt.sign(
-            {
-                id: admin._id,
-                email: admin.email,
-                adminName: admin.adminName,
-                role: admin.role,
-            },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: "1d" }
-        );
-
-        return res.status(200).json({ token });
-
-    } catch (error) {
-        console.error("Error during login:", error);
-        return res.status(500).json({ error: "Server xatosi yuz berdi." });
+    if (admin.role !== "superadmin") {
+      return res.status(403).json({ error: "Sizga ruxsat yo‘q!" });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    admin.lastLogin = new Date();
+    await admin.save();
+
+    const token = jwt.sign(
+      {
+        id: admin._id,
+        email: admin.email,
+        adminName: admin.adminName,
+        role: admin.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Server xatosi yuz berdi." });
+  }
 };
--- Haval/models/Admin.js
+++ Haval/models/Admin.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const Admin = new mongoose.Schema({
-    adminName: { type: String, },
+const Admin = new mongoose.Schema(
+  {
+    adminName: { type: String },
     email: { type: String, unique: true },
-    password: { type: String, },
+    password: { type: String },
     role: { type: String, enum: ["superadmin", "admin"], default: "admin" },
-    status: { type: Number, default: 0 },
+    status: { type: Boolean, default: true },
     lastLogin: { type: Date, default: Date.now },
-},{ timestamps: true });
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Admin", Admin);
